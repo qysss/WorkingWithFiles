69100765f3db579329b4baea01375cfa9c68cfbc
Andrei Papruha <andreipopruha@gmail.com>
2023-01-18 00:21:31 +0400

    qwe

--- src/test/java/papruha/andrei/WorkWithFiles.java
+++ src/test/java/papruha/andrei/WorkWithFiles.java
@@ -4,14 +4,26 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 public class WorkWithFiles {
     @Test
-    void selenideDownloadTest() throws FileNotFoundException {
-        Selenide.open("https://github.com/junit-team/junit5/blob/main/README.md");
+    void selenideDownloadTest() throws Exception {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
         File downloadedFile = $("#raw-url").download();
+        try (InputStream is = new FileInputStream(downloadedFile)) {
+            byte[] bytes = is.readAllBytes();
+            String textContent = new String(bytes, StandardCharsets.UTF_8);
+            assertThat(textContent).contains("This repository is the home of the next generation of JUnit, _JUnit 5_");
+            is.close();
+        }
     }
 }
